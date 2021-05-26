25b3cf64f2dcfa01e0014836bb05ef0f9d35b1a5
ed ben <edwardbenedict92@gmail.com>
2021-05-26 22:16:36 +0300

    feat: add counter page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increase, decrease, reset } from "./redux/actions/actions";
 
 function App() {
+  const { counter } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Counter</h2>
+        <div>
+          <h1>{counter}</h1>
+        </div>
+        <div>
+          <button onClick={() => dispatch(increase())}>increase</button>
+          <button onClick={() => dispatch(reset())}>reset</button>
+          <button onClick={() => dispatch(decrease())}>decrease</button>
+        </div>
       </header>
     </div>
   );
